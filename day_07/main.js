09c55b6757be568637a8374160b6e8c21186ae98
Yann Depps <yann.deperrot@protonmail.com>
2020-07-01 15:45:46 +0200

    number factorial

---
+++ day_07/main.js
@@ -0,0 +1,15 @@
+/*
+  * Number Factorial
+  * Return the factorial of the provided integer.
+  * If the ingeger is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+  * Factorials are often represented with the shorthand notation n.
+  * 5! = 1*2*3*4*5 = 120
+*/
+
+function factorialNumber(num) {
+  // code
+}
+
+const num = 5;
+const result = factorialNumber(num);
+console.log("resutl: ", result);
