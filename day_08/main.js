fd9930f5d6252915af57c834c955a2cbc28d12aa
Yann Depps <yann.deperrot@protonmail.com>
2020-07-03 15:44:20 +0200

    largest Number

--- day_08/main.js
+++ day_08/main.js
@@ -4,9 +4,16 @@
 */
 
 function firstDigit(str) {
-  // code
+  const strNums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const chars = str.split('');
+
+  for (const char of chars) {
+    if (strNums.includes(char)) {
+      return char;
+    }
+  }
 }
 
-const str = 'var_1_Int2';
+const str = 'var_8_Int2';
 const result = firstDigit(str);
 console.log("result: ", result);
---
+++ day_09/main.js
@@ -0,0 +1,12 @@
+/*
+  * Largest Number
+  * For n = 2, the output should be largestNumber(n) = 99
+*/
+
+function largestNumber(num) {
+  // code
+}
+
+const num = 2;
+const result = largestNumber(num);
+console.log("result: ", result);
